f6d5681dfc157219af666dcb97d3a00d054a86b0
Rakshaa Jeyarajah <rakshaa@gmail.com>
2022-03-10 20:30:48 -0500

    l

--- Unit 3/Activity 2/word_jumble.py
+++ Unit 3/Activity 2/word_jumble.py
@@ -1,38 +1,76 @@
 '''
 Rakshaa Jeyarajah
-DAY MONTH YEAR
-DESCRIPTION
+03 03 2022
+Finds all possible ways to rearrange the letters in a word
 '''
 #imports
-import random 
 
 #functions
-def factorial(num):
-    if (num <= 1):
-        return 1
-    else:
-        return num*factorial(num-1)
 
-def word_jumbler(jumble_list, word, shift, max_words):
-    new_word = []
-    if max_words != len(jumble_list):
-        if shift <= len(word):
-            for counter in range(shift, len(word)):
-                new_word.append(word[counter])
-            for counter in range(0, shift):
-                new_word.append(word[counter])
-            if jumble_list.count(''.join(new_word)) == 0:
-                jumble_list.append(''.join(new_word))
-                print(''.join(new_word))
-            word_jumbler(jumble_list, new_word, shift + 1, max_words)
+def letter_swap(jumble_list, shift, swap_one, swap_two):
+    '''
+    @param jumble_list is an array that contains all the different spellings of the original word
+    @param shift is a counter that cunts the allowed shifts
+    @param swap_one is the index of the first letter that will be swapped
+    @param swap_two is the index of the first letter that will be swapped
+    
+    Swaps Two letters in an array. It shifts the the word after swapping the letters and continues
+    shifting and swapping until all ways to rearrange the letters of the given word are done
+    '''
+    if swap_one < len(jumble_list[0]):
+        if swap_two < len(jumble_list[0]):
+            if swap_two == swap_one:
+                letter_swap(jumble_list, shift, swap_one, swap_two + 1)
+            new_word = list(jumble_list[0])
+            element_one = new_word[swap_one]
+            element_two = new_word[swap_two]
+            new_word[swap_one] = element_two
+            new_word[swap_two] = element_one
+            new_word = ''.join(new_word)
+            if jumble_list.count(new_word) == 0:
+                jumble_list.append(new_word)
+                print(new_word)
+            letter_shift(jumble_list, new_word, shift, swap_one, swap_two)
         else:
-            word = list(word)
-            random.shuffle(word)
-            result = ''.join(word)
-            word_jumbler(jumble_list, result, 0, max_words)
+            letter_swap(jumble_list, shift, swap_one + 1, 0)
+    else:
+        return
+
+def letter_shift(jumble_list, word, shift, swap_one, swap_two):
+    '''
+    @param jumble_list is an array that contains all the different spellings of the original word
+    @param word is teh word that will be shifted
+    @param shift is a counter that cunts the allowed shifts
+    @param swap_one is the index of the first letter that will be swapped
+    @param swap_two is the index of the first letter that will be swapped
+    
+    Shifts the letters in a word 1 at a time. After shifting to every possible combination it calls the letter
+    swap function. 
+    '''
+    new_word = []
+    if shift < len(jumble_list[0]):
+        for counter in range(1, len(word)):
+            new_word.append(word[counter])
+        new_word.append(word[0])
+        if jumble_list.count(''.join(new_word)) == 0:
+            jumble_list.append(''.join(new_word))
+            print(''.join(new_word))
+        letter_shift(jumble_list, new_word, shift + 1, swap_one, swap_two)
+    else:
+        letter_swap(jumble_list, 1, swap_one, swap_two + 1)
 
 #main line
-word = "fish"
-jumble_list = [word]
-max = factorial(len(word))
-word_jumbler(jumble_list, word, 0, max)
+while True:
+    user_input = str(input("Enter a word: "))
+    print("By rearranging your word,", user_input, "I have come up with the following \"words\"")
+    jumble_list = [user_input]
+    letter_swap(jumble_list, 1, 0, 0)
+    
+    #Allows the user to rerun the program
+    again = str(input("Would you like to go again(YES/NO)?\n"))
+    while again.upper() != "YES" and again.upper() != "NO":
+        print("Invalid Input! Try again!")
+        again = str(input("Would you like to go again(YES/NO)?\n"))
+    if again.upper() == "NO":
+        break
+print("Good bye!")
---
+++ Unit 4/Activity 2/sorting_routines.py
@@ -0,0 +1,176 @@
+'''
+Rakshaa Jeyarajah
+09 03 2022
+Generates an array and sorts the array based on user given specifications
+'''
+#imports
+import random
+
+#functions
+def selection_sort(data, order):
+    '''
+    @param data is the array that will be sorted
+    @param order will tell the function whether to sort it in ascending or decending order
+    
+    Uses a selection sorting algorithm to sort a numerical array
+    '''
+    for counter in range (0, len(data) - 1):
+        smallest = counter
+        for index in range (counter + 1, len(data)):
+            if order == 1:
+                if data[index] < data[smallest]:
+                    data[index], data[smallest] = data[smallest], data[index] 
+            else:
+                if data[index] > data[smallest]:
+                    data[index], data[smallest] = data[smallest], data[index]
+
+def bubble_sort(data, order):
+    '''
+    @param data is the array that will be sorted
+    @param order will tell the function whether to sort it in ascending or decending order    
+    
+    Uses a bubble sorting algorithm to sort a numerical array
+    '''
+    for counter_one in range(len(data)):
+        for counter_two in range(0,len(data)-1):
+            if order == 1:
+                if data[counter_two] > data[counter_two + 1]:
+                    data[counter_two], data[counter_two + 1] = data[counter_two + 1], data[counter_two] 
+            else:
+                if data[counter_two] < data[counter_two + 1]:
+                    data[counter_two], data[counter_two + 1] = data[counter_two + 1], data[counter_two]
+    return data
+
+
+def insertion_sort(data, order):
+    '''
+    @param data is the array that will be sorted
+    @param order will tell the function whether to sort it in ascending or decending order    
+    
+    Uses a intertion sorting algorithm to sort a numerical array
+    '''
+    for counter in range (1, len(data)): 
+        insert = data[counter]
+        move_item = counter
+
+        if order == 1:
+            while move_item > 0 and data[move_item - 1] > insert:
+                data[move_item] = data[move_item - 1]
+                move_item -= 1
+        else:
+            while move_item > 0 and data[move_item - 1] < insert:
+                data[move_item] = data[move_item - 1]
+                move_item -= 1
+        
+        data[move_item] = insert
+    
+def quick_sort(data, low, high, order):
+    if low < high:
+        split_point = partition(data, low, high, order)
+        quick_sort(data, low, split_point - 1, order)
+        quick_sort(data, split_point + 1, high, order)
+    
+def partition(data, first, last, order): 
+    pivot_value = data[first] 
+    left_mark = first + 1 
+    right_mark = last 
+    done = False
+    while not done:
+        if order == 1:
+            while data[left_mark] <= pivot_value and left_mark <= right_mark:
+                left_mark = left_mark + 1
+
+            while data[right_mark] >= pivot_value and right_mark >= left_mark:
+                right_mark = right_mark - 1
+        else:
+            while data[left_mark] >= pivot_value and left_mark <= right_mark:
+                left_mark = left_mark + 1
+
+            while data[right_mark] <= pivot_value and right_mark >= left_mark:
+                right_mark = right_mark - 1
+
+        if right_mark < left_mark:
+            done = True
+        else:
+            data[left_mark], data[right_mark] = data[right_mark], data[left_mark]
+
+    data[first], data[right_mark] = data[right_mark], data[first]
+  
+    return right_mark 
+
+def random_array_maker(array_length):
+    '''
+    @param array_length is the length of the randomly generated array
+
+    This function generates numbers for an array and adds them to the array. If the randomly generated number
+    is already within the array, a new number will be generated (until a number that is not in the array is generated)
+    and will replace the repeated number
+    '''
+    random_array = []
+    for counter in range(0, array_length):
+        random_array.append(random.randint(0, 1000))
+        while random_array.count(random_array[counter]) > 1:
+            random_array[counter] = random.randint(0, 1000)
+  
+    return random_array
+ 
+#main line
+while True:
+    while True:
+        try:
+            array_length = int(input("How many random numbers do you wish to generate?\n"))
+        except ValueError:
+            print("Invalid Input")
+        else:
+            break
+  
+    while True:
+        try:
+            sort_type = int(input("What type of sort would you like to perform\n1. Selection Sort\n2. Bubble Sort\n3. Intersection Sort\n4. Quick Sort\n"))
+            while sort_type < 1 or sort_type > 4:
+                print("Invalid Input")
+                sort_type = int(input("What type of sort would you like to perform\n1. Selection Sort\n2. Bubble Sort\n3. Intersection Sort\n4. Quick Sort\n"))
+        except ValueError:
+            print("Invalid Input")
+        else:
+            break
+  
+    while True:
+        try:
+            order_type = int(input("In what order would you like the numbers to be sorted?\n1. Ascending\n2. Decending\n"))
+            while order_type != 1 and order_type != 2:
+                print("Invalid Input")
+                order_type = int(input("In what order would you like the numbers to be sorted?\n1. Ascending\n2. Decending\n"))
+        except ValueError:
+            print("Invalid Input")
+        else:
+            break
+    
+    sort_array = random_array_maker(array_length)
+
+    for counter in range (0, array_length): 
+        print(sort_array[counter] , end = " ")
+    
+    print("\n---------------------------------")
+
+    match sort_type:
+        case 1:
+            selection_sort(sort_array, order_type)
+        case 2:
+            bubble_sort(sort_array, order_type)
+        case 3:
+            insertion_sort(sort_array, order_type)
+        case 4:
+            quick_sort(sort_array, 0, array_length, order_type)
+    
+    for counter in range (0, array_length): 
+        print(sort_array[counter] , end = " ")
+
+    again = str(input("Would you like to go again(YES/NO)?\n"))
+    while again.upper() != "YES" and again.upper() != "NO":
+        print("Invalid Input! Try again!")
+        again = str(input("Would you like to go again(YES/NO)?\n"))
+    if again.upper() == "NO":
+        break
+
+print("Good bye!")
